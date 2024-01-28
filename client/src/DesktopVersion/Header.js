304f1c5161ab1b0b2a1d05a8aa962be47d0dc908
codevoke <119005542+codevoke@users.noreply.github.com>
2024-01-28 20:20:46 +0400

    Update Header.js

--- client/src/DesktopVersion/Header.js
+++ client/src/DesktopVersion/Header.js
@@ -1,13 +1,20 @@
 import { Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { Data } from "../App";
 import './css/Header.css';
 
 export default function Header () {
     const navigate = useNavigate();
+    const { user } = useContext(Data);
+    const handleClick = () => {
+        user.auth == true ?
+            navigate("/logout") : navigate("/login")
+    }
     return (
         <div className="Header">
             <div className='title'>Priut</div>
-            <Button variant="outlined" onClick={ () => { navigate("/login"} }>Login</Button>
+            <Button variant="outlined" onClick={handleClick}>{ user.auth == true ? "Logout" : "Login" }</Button>
         </div>
     )
 }
